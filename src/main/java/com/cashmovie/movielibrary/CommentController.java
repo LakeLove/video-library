55c6eacea68d1dbd0ce73323d6f93e07d62d0e45
lakegreene <lakegreene@yahoo.com>
2020-09-01 21:27:01 -0400

    Add error handling to controller

--- src/main/java/com/cashmovie/movielibrary/CommentController.java
+++ src/main/java/com/cashmovie/movielibrary/CommentController.java
@@ -1,35 +1,57 @@
 package com.cashmovie.movielibrary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 public class CommentController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(CommentController.class);
   @Autowired
   CommentService commentService;
   
   @GetMapping (value = "/{video_id}")
   @ResponseBody
   public List<Comment> getVideoComment(@PathVariable ("video_id") Long video_id) {
-    return commentService.findByVideoId(video_id);
+    try {
+      return commentService.findByVideoId(video_id);
+    } catch (Exception e) {
+      LOGGER.info(e.getMessage(), e);
+    }
+    return null;
   }
   
   @GetMapping (value = "/all")
   @ResponseBody
   public List<Comment> getAllComments() {
-    return commentService.findAll();
+    try {
+      return commentService.findAll();
+    } catch (Exception e) {
+      LOGGER.info(e.getMessage(), e);
+    }
+    return null;
   }
   
   @PostMapping (value = "/{video_id}")
   @ResponseBody
   public Comment postVideoComment(@PathVariable ("video_id") Long video_id, @RequestBody Comment comment) {
-    return commentService.save(video_id, comment);
+    try {
+      return commentService.save(video_id, comment);
+    } catch (Exception e) {
+      LOGGER.info(e.getMessage(), e);
+    }
+    return null;
   }
   
   @DeleteMapping (value = "/{video_id}/{comment_id}")
   @ResponseBody
   public void deleteVideoComment(@PathVariable ("video_id") Long video_id, @PathVariable ("comment_id") Long comment_id) {
-    commentService.delete(video_id, comment_id);
+    try {
+      commentService.delete(video_id, comment_id);
+    } catch (Exception e) {
+      LOGGER.info(e.getMessage(), e);
+    }
   }
 }
