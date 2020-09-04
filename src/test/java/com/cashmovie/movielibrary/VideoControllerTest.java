60d58d399ccf249d7fe151959552df9df0403aa7
Frank Hulmes <fdhulmes@yahoo.com>
2020-09-04 11:19:55 -0400

    Better tests for video controller

--- src/test/java/com/cashmovie/movielibrary/VideoControllerTest.java
+++ src/test/java/com/cashmovie/movielibrary/VideoControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.cashmovie.movielibrary.controllers.VideoController;
 import com.cashmovie.movielibrary.entities.Video;
 import com.cashmovie.movielibrary.services.VideoService;
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static sun.plugin2.util.PojoUtil.toJson;
@@ -49,20 +53,45 @@ public class VideoControllerTest {
 
     @Test
     public void testGetVideos() throws Exception {
+        Video testVideo = new Video("Title", "Filepath", "Author", "Description");
+        List<Video> testList = Arrays.asList(testVideo);
+        when(videoService.getRecent()).thenReturn(testList);
         this.mockMvc.perform(get("/api/videos/home").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(buildListJson(testList)))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void testGetSingleVideo() throws Exception {
+        Video testVideo = new Video("Title", "Filepath", "Author", "Description");
+        when(videoService.getVideoById(1L)).thenReturn(testVideo);
         this.mockMvc.perform(get("/api/videos/id=1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(toJson(testVideo)))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void testDeleteVideo() throws Exception {
+        Video testVideo = new Video("Title", "Filepath", "Author", "Description");
+        when(videoService.deleteVideoById(1L)).thenReturn(testVideo);
         this.mockMvc.perform(delete("/api/videos/id=1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(toJson(testVideo)))
                 .andExpect(status().isOk());
+    }
+
+    private String buildListJson(List<Video> list){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        int ind = 0;
+        for(Video v : list){
+            if(ind != 0){
+                sb.append(",");
+            }
+            sb.append(toJson(v));
+            ind++;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
 
