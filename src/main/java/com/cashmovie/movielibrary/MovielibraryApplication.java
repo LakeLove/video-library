ea760db7dd3ac857028dab939d853c17faff7d89
lakegreene <lakegreene@yahoo.com>
2020-09-09 12:18:03 -0400

    Remove AUTH0 identifiers

--- src/main/java/com/cashmovie/movielibrary/MovielibraryApplication.java
+++ src/main/java/com/cashmovie/movielibrary/MovielibraryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovielibraryApplication {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 			SpringApplication.run(MovielibraryApplication.class, args);
 		}
 }
