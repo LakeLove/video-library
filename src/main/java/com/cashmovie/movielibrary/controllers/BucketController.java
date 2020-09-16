494db5f2c507833b5bbc3f8d81accd1342539704
Frank Hulmes <fdhulmes@yahoo.com>
2020-09-16 11:16:58 -0400

    Added an exception for the wrong filetype

--- src/main/java/com/cashmovie/movielibrary/controllers/BucketController.java
+++ src/main/java/com/cashmovie/movielibrary/controllers/BucketController.java
@@ -1,9 +1,12 @@
 package com.cashmovie.movielibrary.controllers;
 
+import com.cashmovie.movielibrary.exceptions.ImproperFileTypeException;
 import com.cashmovie.movielibrary.services.AmazonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin
@@ -20,7 +23,12 @@ public class BucketController {
 
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+      try {
         return this.amazonService.uploadFile(file);
+      } catch (ImproperFileTypeException e) {
+        throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, "Wrong File Type", e);
+      }
     }
 
     @DeleteMapping("/deleteFile")
