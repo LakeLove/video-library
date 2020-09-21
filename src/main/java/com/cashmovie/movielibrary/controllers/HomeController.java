af573af1ffbde0d1ddb34a23bb6b399e92939cdf
lakegreene <lakegreene@yahoo.com>
2020-09-21 14:41:34 -0400

    Remove endpoints to environment variables

--- src/main/java/com/cashmovie/movielibrary/controllers/HomeController.java
+++ src/main/java/com/cashmovie/movielibrary/controllers/HomeController.java
@@ -3,11 +3,7 @@ package com.cashmovie.movielibrary.controllers;
 import com.auth0.AuthenticationController;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.cashmovie.movielibrary.entities.User;
-import com.cashmovie.movielibrary.services.AuthConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.TestingAuthenticationToken;
@@ -15,16 +11,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 @Controller
 @CrossOrigin
 public class HomeController {
-  private static ObjectMapper objectMapper = new ObjectMapper();
+  private static final ObjectMapper objectMapper = new ObjectMapper();
   @Autowired
-  private AuthConfig config;
+  private AuthController authController;
 
   @Autowired
   private AuthenticationController authenticationController;
@@ -36,20 +31,9 @@ public class HomeController {
     if (token != null ) {
       DecodedJWT jwt = JWT.decode(token.getCredentials().toString());
       String user_id = jwt.getClaims().get("sub").asString();
-      return new ObjectMapper().writeValueAsString("Logged in as: "+getUsername(user_id));
+      return objectMapper.writeValueAsString("Logged in as: "+authController.getUsername(user_id));
     } else {
-      return new ObjectMapper().writeValueAsString("You are not logged in.");
+      return objectMapper.writeValueAsString("You are not logged in.");
     }
-  }
-
-  @GetMapping (value = "/api/users/id={user_id}")
-  @ResponseBody
-  protected String getUsername(@PathVariable ("user_id") String user_id) throws IOException, UnirestException {
-    String url = "https://channel-cashmoney.us.auth0.com/api/v2/users/"+user_id+"?fields=username&include_fields=true";
-    HttpResponse<String> response = Unirest.get(url)
-                                           .header("authorization", "Bearer "+config.getApiToken())
-                                           .asString();
-    String responseBody = response.getBody();
-    return new ObjectMapper().readValue(responseBody, User.class).getUsername();
   }
 }
